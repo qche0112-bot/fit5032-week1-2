6435cb5b2651fced7aa1924601022eeb34d56944
Login <qche0112@student.monash.edu>
2025-08-22 14:32:18 +0800

    Modified some details,using the required attribute

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,43 +1,39 @@
 <template>
   <div class="container mt-5">
     <div class="row">
-      <div class="col-12 text-center">
-        <h1 style="white-space: nowrap; text-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5);">
-          User information form/credential
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center">
+          User.information.form
         </h1>
       </div>
     </div>
 
-    <div class="row mt-4">
-      <div class="col-md-4 text-center d-flex align-items-center justify-content-center">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"
-             alt="Vue Logo" style="max-width: 150px;">
-      </div>
+    <div class="row mb-3">
 
       <div class="col-md-8">
         <form @submit.prevent="submitForm">
-          <div class="mb-3">
+          <div class="col-mb-6 col-sm-12">
             <label for="username" class="form-label">username：</label>
-            <input type="text" id="username" class="form-control" v-model="formData.username" required />
+            <input type="text" id="username" class="form-control" required v-model="formData.username" />
           </div>
 
-          <div class="mb-3">
+          <div class="col-mb-6 col-sm-12">
             <label for="password" class="form-label">password：</label>
-            <input type="password" id="password" class="form-control" v-model="formData.password" required />
+            <input type="password" id="password" class="form-control" minlength="4" maxlength="10" v-model="formData.password" required />
           </div>
 
-          <div class="form-check mb-3">
+          <div class="form-check ol-mb-6 col-sm-12">
             <input type="checkbox" id="isAustralian" class="form-check-input" v-model="formData.isAustralian" />
             <label for="isAustralian" class="form-check-label">Australian residents？</label>
           </div>
 
-          <div class="mb-3">
+          <div class="ol-mb-6 col-sm-15">
             <label for="reason" class="form-label">Reasons for joining：</label>
             <textarea id="reason" class="form-control" rows="3" v-model="formData.reason"></textarea>
           </div>
 
-          <div class="mb-3">
-            <label for="gender" class="form-label">Sex：</label>
+          <div class="ol-mb-6 col-sm-12">
+            <label for="gender" class="form-label">Gender：</label>
             <select id="gender" class="form-select" v-model="formData.gender" required>
               <option value="" disabled selected>please selected</option>
               <option value="female">female</option>
