55e9638bb997c4d5652b6c438003df1f7edc2019
Login <qche0112@student.monash.edu>
2025-09-04 15:22:16 +0800

    install primeVUe

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+//import PrimeVue form 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+const app = createApp(App);
+app.use(PrimeVue,{
+    theme:{
+        preset: Aura
+    }
+});
 createApp(App).mount('#app')
